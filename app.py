265535c733037b28f1442111ce3e646d2d19f57f
Lara <lara.portilho.m@gmail.com>
2026-09-05 15:16:47 -0300

    chore: add error for tests

--- app.py
+++ app.py
@@ -18,6 +18,8 @@ load_dotenv()
 
 app = Flask(__name__)
 
+raise Exception('Teste')
+
 # --- Configuração ---
 DATABASE_URL = os.getenv("DATABASE_URL")
 AUTH_SERVICE_URL = os.getenv("AUTH_SERVICE_URL")
